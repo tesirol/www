ff1c2e03e21870b55b1952d53df5250289f42063
james <uptownhr@gmail.com>
2019-11-27 00:20:24 -0800

    Only loading logrocket in production

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,18 @@
+
+function plugins (env) {
+  const plugins = []
+
+  const prodOnly = [{src: '~/plugins/log-rocket.js', mode: 'client'}]
+
+  if (env == 'production') plugins.push(prodOnly)
+
+  return plugins
+}
+
+const plugins = plugins(process.env.NODE_ENV)
+
 export default {
+  plugins,
   mode: 'universal',
-  srcDir: 'src',
-  plugins: [
-    {src: '~/plugins/clientInit.js', mode: 'client'}
-  ]
+  srcDir: 'src'
 }
